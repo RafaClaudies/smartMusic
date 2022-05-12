396573b6617d065ef1da230b4b495512e4941264
RafaCla <rafaclaudies@outlook.com>
2022-05-11 19:40:33 -0500

    [NEW] - Create fragments

--- app/src/main/java/com/example/smartweather/views/HomeActivity.kt
+++ app/src/main/java/com/example/smartweather/views/HomeActivity.kt
@@ -2,16 +2,25 @@ package com.example.smartweather.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.smartweather.R
+import com.example.smartweather.databinding.ActivityHomeBinding
 import com.example.smartweather.viewModel.HomeViewModel
 
 class HomeActivity : AppCompatActivity() {
 
     private val homeViewModel by lazy { ViewModelProvider(this).get(HomeViewModel::class.java) }
+    lateinit var bindingView: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        bindingView = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        bindingView.apply {
+            lifecycleOwner = this@HomeActivity
+        }
+
     }
 }
